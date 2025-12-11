22be49eeab8803dc785119e346bd4f751ee4b4ce
johnjun4 <johnsong73965@gmail.com>
2025-12-11 22:44:25 +0800

    init reset

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Invoices Page1111</p>;
+  return <p>Invoices Page</p>;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Dashboard Page4444</p>;
+  return <p>Dashboard Page</p>;
 }
