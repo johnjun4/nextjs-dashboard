2d2fd44dbd2f4d69f71ab6632f8c21e59cbfcfa0
johnjun4 <johnsong73965@gmail.com>
2025-12-11 22:42:30 +0800

    test

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Invoices Page111111</p>;
+  return <p>Invoices Page1111</p>;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Dashboard Page333</p>;
+  return <p>Dashboard Page4444</p>;
 }
